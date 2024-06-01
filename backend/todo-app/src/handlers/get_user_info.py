0f430ca4abde44b875f419c18c0b530a2cef28ac
Koujirou513 <kojiro323a@gmail.com>
2024-06-01 11:20:00 +0900

    [add]レスポンスヘッダ追加、CORS対応

--- backend/todo-app/src/handlers/get_user_info.py
+++ backend/todo-app/src/handlers/get_user_info.py
@@ -135,6 +135,11 @@ def get_user_info_handler(event, context):
         
         return {
             'statusCode': 200,
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,OPTIONS,PUT,POST,DELETE",
+                "Access-Control-Allow-Headers": "Content-Type,",
+            },
             'body': json.dumps(result, cls=DecimalEncoder)
         }
         
